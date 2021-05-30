1dfb24cfc51a09e51773c219f4f10e99e2216589
Mahmoud Fahiem <xmahmoud92@gmail.com>
2021-05-30 07:13:22 +0200

    Define UI Variables

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const taskForm = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const taskFilter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task ');
